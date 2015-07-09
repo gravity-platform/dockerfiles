0e140b6a2e3496326f248b22ede60e52d072c473
Lucas Bickel <lucas.bickel@swisscom.com>
2015-07-09 19:25:48 +0200

    cleanup code and add example config

--- jenkins-autojobs/entrypoint.py
+++ jenkins-autojobs/entrypoint.py
@@ -3,18 +3,17 @@
 import sys, subprocess
 
 class Entrypoint:
-        def main(self):
-                if len(sys.argv) < 2:
-                        self.error()
-                if sys.argv[1] not in ['git', 'hg', 'svn']:
-                        self.error()
+    def main(self):
+        if len(sys.argv) < 2:
+            self.error()
+        if sys.argv[1] not in ['git', 'hg', 'svn']:
+            self.error()
 
-                print(['/usr/local/bin/jenkins-makejobs-' + sys.argv[1]] + sys.argv[2:])
-                subprocess.call(['/usr/local/bin/jenkins-makejobs-' + sys.argv[1]] + sys.argv[2:] + ['/config.yml'])
-        def error(self):
-                print('  Usage: docker run -v `pwd`/config.yml:/config.yml gravity-platform/jenkins-autojobs <git|svn|hg> [autojobs-args]')
-                sys.exit(2)
+        subprocess.call(['/usr/local/bin/jenkins-makejobs-' + sys.argv[1]] + sys.argv[2:] + ['/config.yml'])
+    def error(self):
+        print('  Usage: docker run -v `pwd`/config.yml:/config.yml gravity-platform/jenkins-autojobs <git|svn|hg> [autojobs-args]')
+        sys.exit(2)
 
 
 if __name__ == '__main__':
-        Entrypoint().main()
+    Entrypoint().main()
